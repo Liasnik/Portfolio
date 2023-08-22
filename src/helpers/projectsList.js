c9d4197664dff6242c7f94e8f6da4999a74770d1
Alexandr <9426247@gmail.com>
2023-08-22 23:32:50 +0300

    first commit

---
+++ src/helpers/projectsList.js
@@ -0,0 +1,62 @@
+import project01 from './../img/projects/Silver-Desire-small.png'
+import project01Big from './../img/projects/Silver-Desire.png'
+
+import project02 from './../img/projects/React-Tutorial-small.png'
+import project02Big from './../img/projects/React-Tutorial.png'
+
+import project03 from './../img/projects/Sorter-small.png'
+import project03Big from './../img/projects/Sorter.png'
+
+import project04 from './../img/projects/04.jpg'
+import project04Big from './../img/projects/04-big.jpg'
+
+import project05 from './../img/projects/05.jpg'
+import project05Big from './../img/projects/05-big.jpg'
+
+import project06 from './../img/projects/Todo-app.png'
+import project06Big from './../img/projects/Todo-app.png'
+
+const projects = [
+  {
+    title: 'Jewelry Store',
+    skills: 'React, Redux',
+    img: project01,
+    imgBig: project01Big,
+    gitHubLink: 'https://liasnik.github.io/Silver-Desire/',
+  },
+  {
+    title: 'React Tutorial path 4',
+    img: project02,
+    imgBig: project02Big,
+    skills: 'React',
+    gitHubLink: 'https://liasnik.github.io/React-04-Escape-Hatches',
+  },
+  {
+    title: 'List and table handler',
+    img: project03,
+    imgBig: project03Big,
+    skills: 'HTML, CSS, JavaScript',
+    gitHubLink: 'https://liasnik.github.io/Sorter',
+  },
+  {
+    title: 'Dating app',
+    img: project04,
+    imgBig: project04Big,
+    skills: 'React Native',
+  },
+  {
+    title: 'Landing',
+    img: project05,
+    imgBig: project05Big,
+    skills: 'HTML, SCSS, JS',
+  },
+  {
+    title: 'Todo App',
+    img: project06,
+    imgBig: project06Big,
+    skills: 'React, Redux Toolkit',
+    gitHubLink: 'https://liasnik.github.io/Todo-App',
+  },
+]
+
+export { projects }
